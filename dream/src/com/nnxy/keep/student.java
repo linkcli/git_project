bec6af5bb5b039903b0bd2d9bafd38491ffb0a62
linkcli <2079965823@qq.com>
2019-05-31 10:57:49 +0800

    eclipse第一次提交

---
+++ dream/src/com/nnxy/keep/student.java
@@ -0,0 +1,20 @@
+package com.nnxy.keep;
+
+public class student {
+   String name;
+   static int age;
+   public static void main(String[] args) {
+	   if(age != 0) {
+	    for(int i=0; i<age; i++) {
+	     System.out.print("Thanks your coming");
+	     if(age>0) {
+	      System.out.print("intput error");
+	               }
+	                             }
+	                }
+	                                      }
+	                 }
+  
+   
+
+
